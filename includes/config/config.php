1da7cac9eacb3888d4a7f563d8784019331d75c6
Paul <paulchrisycj@gmail.com>
2019-10-10 23:01:10 +0800

    Added API, API Configs and API Dump file

---
+++ includes/config/config.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chester Chin
+ * Date: 4/13/2017
+ * Time: 9:24 AM
+ */
+ 
+
+
+// define('dbName', 'ks_test');
+// define('userName', 'root');
+// define('passWord', '');
+// define('dsn','mysql:host=127.0.0.1;dbname=ks_test');
+
+
+//Use for local dev 
+if ($_SERVER['SERVER_NAME']=="localhost"){
+
+    define('dbName', 'agiledev');
+    define('userName', 'root');
+    define('passWord', '');
+    define('dsn','mysql:host=127.0.0.1;dbname=agiledev');
+    
+    
+
+}
+//Use for live site
+elseif ($_SERVER['SERVER_NAME']==""){
+
+    define('dbName', '');
+    define('userName', '');
+    define('passWord', '');
+    define('dsn','mysql:host=127.0.0.1;dbname=');
+    
+    
+}
+else{
+    echo "Current server is not configured to access KS database.";
+}
+
+
---
+++ includes/server/testing.php
@@ -0,0 +1,5 @@
+<?php
+echo "<a href='index.php?action=loginAdmin&admin_username=paul&admin_password=111111'>Test Login</a><br>";
+echo "<a href='index.php?action=loginAdmin&admin_username=paul&admin_password=1111'>Test Login Fail</a><br>";
+echo "<a href='index.php?action=showAllBooking'>Show All Booking</a><br>";
+?>
